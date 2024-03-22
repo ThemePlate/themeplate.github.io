163d0b6807f6b09c2969b246190d814b22c0f060
kermage <genealyson.torcende@gmail.com>
2024-03-23 06:38:17 +0800

    clean showcase

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,19 @@
 export default function () {
 	return (
-		<h1 className="text-center text-5xl md:text-6xl font-black tracking-wide">ThemePlate Fields</h1>
+		<div className="grid place-items-center gap-8 md:gap-16 lg:w-full">
+			<h1 className="text-center text-5xl font-black tracking-wide md:text-6xl">ThemePlate Fields</h1>
+
+			<div class="flex items-center gap-4">
+				<a href="https://vitejs.dev" target="_blank">
+					<img src="/vite.svg" alt="Vite logo" />
+				</a>
+				<a href="https://preactjs.com" target="_blank">
+					<img src="/preact.svg" alt="Preact logo" />
+				</a>
+				<a href="https://www.typescriptlang.org" target="_blank">
+					<img src="/typescript.svg" alt="Typescript logo" />
+				</a>
+			</div>
+		</div>
 	);
 }
